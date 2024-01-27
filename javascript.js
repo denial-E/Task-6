02db3a17093aa950875b1750faa325be8545f9ba
denial-E <shempeter221992@gmail.com>
2024-01-28 01:28:27 +0530

    run bro

--- javascript.js
+++ javascript.js
@@ -1,13 +1,27 @@
 class Movie {
-    constructor(title, studio, rating) {
+    constructor(title, studio, rating = "PG") {
         this.title = title;
         this.studio = studio;
         this.rating = rating;
     }
+
+    static getPG(movies) {
+        return movies.filter(movie => movie.rating === "PG");
+    }
 }
 
+// Creating an instance of Movie
+const casinoRoyale = new Movie("Casino Royale", "Eon Productions", "PG-13");
 
-let movie1 = new Movie("Inception", "Warner Bros.", "PG-13");
-console.log(movie1.title);   
-console.log(movie1.studio);  
-console.log(movie1.rating);   
+// Example array of movies
+const movies = [
+    new Movie("Movie 1", "Studio 1", "PG"),
+    new Movie("Movie 2", "Studio 2", "R"),
+    new Movie("Movie 3", "Studio 3", "PG"),
+    new Movie("Movie 4", "Studio 4", "PG-13"),
+    new Movie("Movie 5", "Studio 5", "G")
+];
+
+// Getting PG-rated movies from the array
+const pgMovies = Movie.getPG(movies);
+console.log(pgMovies);
